9af8b8a24cd66b55e6c304370089a6b00f218e0f
wawrze <mateusz.wawreszuk@gmail.com>
2018-06-24 19:51:14 +0200

    board printing

---
+++ src/main/java/sudoku/user_interface/ConsoleInterface.java
@@ -0,0 +1,66 @@
+package sudoku.user_interface;
+
+import sudoku.SudokuBoard;
+import sudoku.SudokuElement;
+
+public class ConsoleInterface {
+
+    private static boolean simplePrint = true;
+
+    public static void printBoard(SudokuBoard board) {
+        if(simplePrint) {
+            printBoardSimple(board);
+            return;
+        }
+        System.out.println("┏━━━┯━━━┯━━━┳━━━┯━━━┯━━━┳━━━┯━━━┯━━━┓");
+        for(int i = 1;i < 10;i++) {
+            printRow(board, i);
+            if(i == 9)
+                System.out.println("┗━━━┷━━━┷━━━┻━━━┷━━━┷━━━┻━━━┷━━━┷━━━┛");
+            else if(i == 3 || i == 6)
+                System.out.println("┣━━━┿━━━┿━━━╋━━━┿━━━┿━━━╋━━━┿━━━┿━━━┫");
+            else
+                System.out.println("┠───┼───┼───╂───┼───┼───╂───┼───┼───┨");
+        }
+    }
+
+    private static void printBoardSimple(SudokuBoard board) {
+        for(int i = 1;i < 10;i++){
+            System.out.println("+---+---+---+---+---+---+---+---+---+");
+            printRowSimple(board, i);
+        }
+        System.out.println("+---+---+---+---+---+---+---+---+---+");
+    }
+
+    private static void printRow(SudokuBoard board, int row) {
+        System.out.print("┃");
+        for(int i = 1;i < 10;i++){
+            if(board.getValue(row, i) == SudokuElement.EMPTY)
+                System.out.print("   ");
+            else
+                System.out.print(" " + board.getValue(row, i) + " ");
+            if((i % 3) == 0)
+                System.out.print("┃");
+            else
+                System.out.print("│");
+        }
+        System.out.println();
+    }
+
+    private static void printRowSimple(SudokuBoard board, int row) {
+        System.out.print("|");
+        for(int i = 1;i < 10;i++){
+            if(board.getValue(row, i) == SudokuElement.EMPTY)
+                System.out.print("   ");
+            else
+                System.out.print(" " + board.getValue(row, i) + " ");
+            System.out.print("|");
+        }
+        System.out.println();
+    }
+
+    public static void switchSimplePrint() {
+        simplePrint = !simplePrint;
+    }
+
+}
---
+++ src/test/java/sudoku/user_interface/ConsoleInterfaceTestSuite.java
@@ -0,0 +1,31 @@
+package sudoku.user_interface;
+
+import org.junit.Test;
+import sudoku.SudokuBoard;
+import sudoku.SudokuGame;
+import sudoku.exceptions.IncorrectValueException;
+
+public class ConsoleInterfaceTestSuite {
+
+    @Test
+    public void testPrintBoard() throws IncorrectValueException {
+        //Given
+        SudokuGame game = new SudokuGame();
+        SudokuBoard board = game.getBoard();
+        //When
+        game.setValue(1, 1, 1);
+        game.setValue(2, 5, 2);
+        game.setValue(3, 9, 3);
+        game.setValue(4, 9, 4);
+        game.setValue(5, 5, 5);
+        game.setValue(6, 1, 6);
+        game.setValue(7, 5, 7);
+        game.setValue(8, 1, 8);
+        game.setValue(9, 9, 9);
+        ConsoleInterface.printBoard(board);
+        ConsoleInterface.switchSimplePrint();
+        ConsoleInterface.printBoard(board);
+        //Then
+    }
+
+}
